1038cc69d4636e9c4faffb06059ff9cf0231d397
atrombetone <atrombetone@ciandt.com>
2020-03-30 14:08:25 -0300

    ajustes de layout da home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { SprintsComponent } from './pages/sprints/sprints.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
+import { MenuItem } from './models/menu-item';
 
 const routes: Routes = [
   { path: 'projects', component: ProjectsComponent },
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,20 +2,30 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { Subscription } from 'rxjs';
 import { MenuItem } from 'src/app/models/menu-item';
+import { ActivatedRoute, Router, ActivationEnd, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'page-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, On {
 
   subscription: Subscription;
   menus: Array<MenuItem> = [];
+  menu: MenuItem;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private route: Router) {
+    route.events.subscribe( event => {
+
+      if (event instanceof ActivationEnd) {
+          this.menu = ((event as ActivationEnd).snapshot.params) as MenuItem;
+      }
+    });
+   }
   
   ngOnInit() {
+  
     this.subscription = this.loginService.getMenus().subscribe(data => {
       this.menus = data;
     });
